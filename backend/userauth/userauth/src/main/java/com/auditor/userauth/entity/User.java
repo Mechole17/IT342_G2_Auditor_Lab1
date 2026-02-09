fc05269b43dd70e76786870f3dc63b325f3736c5
LAB24_USER ON WS44 <L24X07W44@LabHCI.net>
2026-02-09 08:57:52 +0800

    backend(entity): added user entity

---
+++ backend/userauth/userauth/src/main/java/com/auditor/userauth/entity/User.java
@@ -0,0 +1,27 @@
+package com.auditor.userauth.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userid;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+
+
+}
